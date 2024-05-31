f368dcbf436a522a56de881d6df29f42025c941f
Jad Seifeddine <jseifeddine@macquarietelecom.com>
2024-05-31 19:52:40 +1000

    jsonize junos output

--- lib/oxidized/model/junos.rb
+++ lib/oxidized/model/junos.rb
@@ -1,3 +1,5 @@
+require 'json'
+
 class JunOS < Oxidized::Model
   using Refinements
   comment '# '
@@ -6,45 +8,74 @@ class JunOS < Oxidized::Model
     @input.class.to_s.match(/Telnet/)
   end
 
-  cmd :all do |cfg|
-    cfg = cfg.cut_both if screenscrape
-    cfg.gsub!(/  scale-subscriber (\s+)(\d+)/, '  scale-subscriber                <count>')
-    cfg.gsub!(/VMX-BANDWIDTH\s+(\d+) (.*)/, 'VMX-BANDWIDTH                  <count> \2')
-    cfg.lines.map { |line| line.rstrip }.join("\n") + "\n"
+  def sanitize_json(json_obj)
+    case json_obj
+    when Hash
+      json_obj.each do |key, value|
+        if key == 'name' && json_obj.key?('community')
+          json_obj[key] = '<secret removed>'
+        elsif key == 'secret'
+          json_obj[key] = '<secret removed>'
+        else
+          sanitize_json(value)
+        end
+      end
+    when Array
+      json_obj.each { |item| sanitize_json(item) }
+    end
   end
 
-  cmd :secret do |cfg|
-    cfg.gsub!(/community (\S+) {/, 'community <hidden> {')
-    cfg.gsub!(/(ssh-(rsa|dsa|ecdsa|ecdsa-sk|ed25519|ed25519-sk) )".*; ## SECRET-DATA/, '<secret removed>')
-    cfg.gsub!(/ "\$\d\$\S+; ## SECRET-DATA/, ' <secret removed>;')
-    cfg
+  def safe_parse_json(json_str)
+    JSON.parse(json_str.encode('UTF-8', 'binary', invalid: :replace, undef: :replace, replace: ''))
   end
 
-  cmd 'show version' do |cfg|
+  cmd 'show version | display json' do |cfg|
     @model = Regexp.last_match(1) if cfg =~ /^Model: (\S+)/
-    comment cfg
+    version_info = safe_parse_json(cfg)
+    sanitize_json(version_info)
+    @outputs ||= {}
+    @outputs['version'] = version_info
+    ''
   end
 
   post do
-    out = ''
+    @outputs ||= {}
+
     case @model
     when 'mx960'
-      out << cmd('show chassis fabric reachability') { |cfg| comment cfg }
+      out = cmd('show chassis fabric reachability | display json')
+      chassis_fabric_reachability = safe_parse_json(out)
+      sanitize_json(chassis_fabric_reachability)
+      @outputs['chassis-fabric-reachability'] = chassis_fabric_reachability
     when /^(ex22|ex3[34]|ex4|ex8|qfx)/
-      out << cmd('show virtual-chassis') { |cfg| comment cfg }
+      out = cmd('show virtual-chassis | display json')
+      virtual_chassis = safe_parse_json(out)
+      sanitize_json(virtual_chassis)
+      @outputs['virtual-chassis'] = virtual_chassis
     end
-    out
-  end
 
-  cmd('show chassis hardware') { |cfg| comment cfg }
-  cmd('show system license') do |cfg|
-    cfg.gsub!(/  fib-scale\s+(\d+)/, '  fib-scale                       <count>')
-    cfg.gsub!(/  rib-scale\s+(\d+)/, '  rib-scale                       <count>')
-    comment cfg
-  end
-  cmd('show system license keys') { |cfg| comment cfg }
+    out = cmd('show chassis hardware | display json')
+    chassis_hardware = safe_parse_json(out)
+    sanitize_json(chassis_hardware)
+    @outputs['chassis-hardware'] = chassis_hardware
 
-  cmd "show configuration | display #{@display_format == 'xml' || @display_format == 'json' || @display_format == 'set' ? @display_format : 'omit'}"
+    out = cmd('show system license | display json')
+    system_license = safe_parse_json(out)
+    sanitize_json(system_license)
+    @outputs['system-license'] = system_license
+
+    out = cmd('show system license keys | display json')
+    system_license_keys = safe_parse_json(out)
+    sanitize_json(system_license_keys)
+    @outputs['system-license-keys'] = system_license_keys
+
+    out = cmd('show configuration | display json')
+    configuration = safe_parse_json(out)['configuration'] # the configuration comes inside a 'configuration' object, bit dumb...
+    sanitize_json(configuration)
+    @outputs['configuration'] = configuration
+
+    JSON.pretty_generate(@outputs)
+  end
 
   cfg :telnet do
     username(/^login:/)
