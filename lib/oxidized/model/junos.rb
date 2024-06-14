2d80d70862a496c20716049e273df624e76ca6c8
Jad Seifeddine <jadseifeddine@macquarietelecom.com>
2024-06-14 18:10:14 +1000

    renmed the junos.rb to junos-json.rb and replaced junos.rb with original, with changes to handle encoding issues

--- lib/oxidized/model/junos.rb
+++ lib/oxidized/model/junos.rb
@@ -6,74 +6,53 @@ class JunOS < Oxidized::Model
     @input.class.to_s.match(/Telnet/)
   end
 
-  def sanitize_json(json_obj)
-    case json_obj
-    when Hash
-      json_obj.each do |key, value|
-        if key == 'name' && json_obj.key?('community')
-          json_obj[key] = '<secret removed>'
-        elsif key == 'secret'
-          json_obj[key] = '<secret removed>'
-        else
-          sanitize_json(value)
-        end
-      end
-    when Array
-      json_obj.each { |item| sanitize_json(item) }
-    end
+  def sanitize_encoding(cfg)
+    cfg.force_encoding('UTF-8').scrub('')
   end
 
-  def safe_parse_json(json_str)
-    JSON.parse(json_str.encode('UTF-8', 'binary', invalid: :replace, undef: :replace, replace: ''))
+  cmd :all do |cfg|
+    cfg = cfg.cut_both if screenscrape
+    cfg = sanitize_encoding(cfg)
+    cfg.gsub!(/  scale-subscriber (\s+)(\d+)/, '  scale-subscriber                <count>')
+    cfg.gsub!(/VMX-BANDWIDTH\s+(\d+) (.*)/, 'VMX-BANDWIDTH                  <count> \2')
+    cfg.lines.map { |line| line.rstrip }.join("\n") + "\n"
   end
 
-  cmd 'show version | display json' do |cfg|
+  cmd :secret do |cfg|
+    cfg = sanitize_encoding(cfg)
+    cfg.gsub!(/community (\S+) {/, 'community <hidden> {')
+    cfg.gsub!(/(ssh-(rsa|dsa|ecdsa|ecdsa-sk|ed25519|ed25519-sk) )".*; ## SECRET-DATA/, '<secret removed>')
+    cfg.gsub!(/ "\$\d\$\S+; ## SECRET-DATA/, ' <secret removed>;')
+    cfg
+  end
+
+  cmd 'show version' do |cfg|
+    cfg = sanitize_encoding(cfg)
     @model = Regexp.last_match(1) if cfg =~ /^Model: (\S+)/
-    version_info = safe_parse_json(cfg)
-    sanitize_json(version_info)
-    @outputs ||= {}
-    @outputs['version'] = version_info
-    ''
+    comment cfg
   end
 
   post do
-    @outputs ||= {}
-
+    out = ''
     case @model
     when 'mx960'
-      out = cmd('show chassis fabric reachability | display json')
-      chassis_fabric_reachability = safe_parse_json(out)
-      sanitize_json(chassis_fabric_reachability)
-      @outputs['chassis-fabric-reachability'] = chassis_fabric_reachability
+      out << cmd('show chassis fabric reachability') { |cfg| comment sanitize_encoding(cfg) }
     when /^(ex22|ex3[34]|ex4|ex8|qfx)/
-      out = cmd('show virtual-chassis | display json')
-      virtual_chassis = safe_parse_json(out)
-      sanitize_json(virtual_chassis)
-      @outputs['virtual-chassis'] = virtual_chassis
+      out << cmd('show virtual-chassis') { |cfg| comment sanitize_encoding(cfg) }
     end
-
-    out = cmd('show chassis hardware | display json')
-    chassis_hardware = safe_parse_json(out)
-    sanitize_json(chassis_hardware)
-    @outputs['chassis-hardware'] = chassis_hardware
-
-    out = cmd('show system license | display json')
-    system_license = safe_parse_json(out)
-    sanitize_json(system_license)
-    @outputs['system-license'] = system_license
-
-    out = cmd('show system license keys | display json')
-    system_license_keys = safe_parse_json(out)
-    sanitize_json(system_license_keys)
-    @outputs['system-license-keys'] = system_license_keys
-
-    out = cmd('show configuration | display json')
-    configuration = safe_parse_json(out)['configuration'] # the configuration comes inside a 'configuration' object, bit dumb...
-    sanitize_json(configuration)
-    @outputs['configuration'] = configuration
-
-    JSON.pretty_generate(@outputs)
+    out
   end
+
+  cmd('show chassis hardware') { |cfg| comment sanitize_encoding(cfg) }
+  cmd('show system license') do |cfg|
+    cfg = sanitize_encoding(cfg)
+    cfg.gsub!(/  fib-scale\s+(\d+)/, '  fib-scale                       <count>')
+    cfg.gsub!(/  rib-scale\s+(\d+)/, '  rib-scale                       <count>')
+    comment cfg
+  end
+  cmd('show system license keys') { |cfg| comment sanitize_encoding(cfg) }
+
+  cmd 'show configuration | display omit'
 
   cfg :telnet do
     username(/^login:/)
